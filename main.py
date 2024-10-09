59228d3fd96d2c9839289d26818347006dc8355c
abyssalCodewright <zerosie@gmail.com>
2024-10-09 11:25:49 -0500

    window solution fix; drawing lines

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
-from tkinter import Tk, BOTH, Canvas
-
-class Window:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.root = Tk()
-        self.root.title("Maze Solver")
-        self.canvas = Canvas(self.root, bg="black")
-        self.canvas.pack()
-        self.running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
-
-    def redraw(self):
-        self.root.update_idletasks()
-        self.root.update()
-
-    def wait_for_close(self):
-        self.running = True
-
-        while self.running == True:
-            self.redraw()
-
-    def close(self):
-        self.running = False
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(50,400), Point(400,400))
+    win.draw_line(line, "white")
     win.wait_for_close()
 
 main()
